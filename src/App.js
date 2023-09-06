f12ae45a1a69a4c737531bff7509c904626dc01d
marijana29 <126325944+marijana29@users.noreply.github.com>
2023-09-06 13:33:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends Component {
 
    onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-   fetch('http://localhost:5000/imageurl', {
+   fetch('https://mybackend-qk20.onrender.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -87,7 +87,7 @@ class App extends Component {
     .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:5000/image', {
+          fetch('https://mybackend-qk20.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
