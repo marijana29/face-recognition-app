a70b0438879307fa2c8038e3e2e06f591c6d20b7
marijana29 <126325944+marijana29@users.noreply.github.com>
2023-09-11 14:11:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
 import './App.css';
 
-
-
 const initialState = {
   input: '',
   imageUrl: '',
@@ -32,19 +30,18 @@ class App extends Component {
     this.state = initialState;
   }
 
- loadUser = (data) => {
-  this.setState((prevState) => ({
-    user: {
-      ...prevState.user,
-      id: data.id,
-      name: data.name,
-      email: data.email,
-       entries: data.entries,
-      joined: data.joined,
-    }
-  }));
-
-  
+  loadUser = (data) => {
+    this.setState((prevState) => ({
+      user: {
+        ...prevState.user,
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      }
+    }));
+  };
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -54,8 +51,8 @@ class App extends Component {
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
     };
   }
 
@@ -67,91 +64,89 @@ class App extends Component {
     this.setState({ input: event.target.value });
   }
 
-onButtonSubmit = () => {
-  this.setState({ imageUrl: this.state.input });
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
 
-  const raw = JSON.stringify({
-    "user_app_id": {
-      "user_id": "marijana29",
-      "app_id": "facerecognitionbrain"
-    },
-    "inputs": [
-      {
-        "data": {
-          "image": {
-            "url": this.state.input
+    const raw = JSON.stringify({
+      "user_app_id": {
+        "user_id": "marijana29",
+        "app_id": "facerecognitionbrain"
+      },
+      "inputs": [
+        {
+          "data": {
+            "image": {
+              "url": this.state.input
+            }
           }
         }
-      }
-    ]
-  });
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Authorization': 'Key 6399446788964a84813fa6a92d82ca0d'
-    },
-    body: raw
-  };
-
-  fetch("https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs", requestOptions)
-    .then(response => response.json())
-    .then(data => {
-      try {
-        // Attempt to access face location information
-        const faceData = data.outputs[0].data.regions[0].region_info.bounding_box;
-
-        // Check if faceData is defined
-        if (faceData) {
-          const image = document.getElementById('inputimage');
-          const width = Number(image.width);
-          const height = Number(image.height);
-
-          // Extract face location information
-          const faceLocation = {
-            leftCol: faceData.left_col * width,
-            topRow: faceData.top_row * height,
-            rightCol: width - (faceData.right_col * width),
-            bottomRow: height - (faceData.bottom_row * height)
-          };
-
-          // Display the face box
-          this.displayFaceBox(faceLocation);
-
-          // Update the user's entry count
-          this.setState(prevState => ({
-            user: {
-              ...prevState.user,
-            entries: parseInt(prevState.user.entries, 10) + 1 
-            }
-          }));
-        } else {
-          console.log('No face detected');
-        }
-      } catch (error) {
-        console.log('Error processing response:', error);
-      }
-    })
-    .catch(error => {
-      console.log('error', error);
-      // Handle error
+      ]
     });
-}
 
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key 6399446788964a84813fa6a92d82ca0d'
+      },
+      body: raw
+    };
 
+    fetch("https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs", requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        try {
+          // Attempt to access face location information
+          const faceData = data.outputs[0].data.regions[0].region_info.bounding_box;
+
+          // Check if faceData is defined
+          if (faceData) {
+            const image = document.getElementById('inputimage');
+            const width = Number(image.width);
+            const height = Number(image.height);
+
+            // Extract face location information
+            const faceLocation = {
+              leftCol: faceData.left_col * width,
+              topRow: faceData.top_row * height,
+              rightCol: width - faceData.right_col * width,
+              bottomRow: height - faceData.bottom_row * height
+            };
+
+            // Display the face box
+            this.displayFaceBox(faceLocation);
+
+            // Update the user's entry count
+            this.setState(prevState => ({
+              user: {
+                ...prevState.user,
+                entries: parseInt(prevState.user.entries, 10) + 1 
+              }
+            }));
+          } else {
+            console.log('No face detected');
+          }
+        } catch (error) {
+          console.log('Error processing response:', error);
+        }
+      })
+      .catch(error => {
+        console.log('error', error);
+        // Handle error
+      });
+  }
 
   onRouteChange = (route) => {
     if (route === 'signout') {
       this.setState(initialState)
     } else if (route === 'home') {
-      this.setState({isSignedIn: true})
+      this.setState({ isSignedIn: true })
     }
-    this.setState({route: route});
+    this.setState({ route: route });
   }
-   
+
   render() {
-    const { isSignedIn, imageUrl, route, box} = this.state;
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <ParticlesBg color="#FFFFFF" num={200} type="cobweb" bg={true} />
